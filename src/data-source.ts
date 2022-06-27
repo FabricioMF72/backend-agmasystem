707aa09921439c13d8cf377a5c1ffad71e1185c1
Fabricio Murillo Flores <Fabriciomf72@gmail.com>
2022-06-26 21:13:49 -0600

    [CHANGE] DataSource config

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,8 +4,8 @@ import { Client } from "./entity/Client"
 
 export const AppDataSource = new DataSource({
     type: "mssql",
-    host: "tcp:agamadev.database.windows.net",
-    //port: 1433,
+    host: "agamadev.database.windows.net",
+    port: 1433,
     username: "AdminAgma@agamadev.database.windows.net",
     password: "#6X4VjF&qAr",
     database: "agmadevdb",
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-// import { AppDataSource } from "./data-source"
-// import { Client } from "./entity/Client"
+import { AppDataSource } from "./data-source"
+import { Client } from "./entity/Client"
 
-// AppDataSource.initialize().then(async () => {
+AppDataSource.initialize().then(async () => {
 
-//     // console.log("Inserting a new user into the database...")
-//     // const client = new Client()
-//     // client.FirstName = "Timber"
-//     // client.LastName = "Saw"
-//     // await AppDataSource.manager.save(client)
-//     // console.log("Saved a new user with id: " + client.ClientID)
+  console.log("Inserting a new user into the database...")
+  const client = new Client()
+  client.FirstName = "Timber"
+  client.LastName = "Saw"
+  await AppDataSource.manager.save(client)
+  console.log("Saved a new user with id: " + client.ClientID)
 
-//     console.log("Loading users from the database...")
-//     const clients = await AppDataSource.manager.find(Client)
-//     console.log("Loaded users: ", clients)
+  console.log("Loading users from the database...")
+  const clients = await AppDataSource.manager.find(Client)
+  console.log("Loaded users: ", clients)
 
-//     console.log("Here you can setup and run express / fastify / any other framework.")
+  console.log("Here you can setup and run express / fastify / any other framework.")
 
-// }).catch(error => console.log(error))
+}).catch(error => console.log(error))
 
-var ISOLATION_LEVEL = require('tedious').ISOLATION_LEVEL;
+/*var ISOLATION_LEVEL = require('tedious').ISOLATION_LEVEL;
 const { Connection } = require("tedious");
 
 // Create connection to database
@@ -50,4 +50,4 @@ connection.on("connect", (err: { message: any; }) => {
   }
 });
 
-connection.connect();
+connection.connect();*/
